72ff131ee37d77cc2e3205358bf2ec0db0c51114
Podsnezhznik <150585139+Podsnezhznik@users.noreply.github.com>
2024-10-02 20:50:28 +0400

    Добавьте файлы проекта.

---
+++ IB_1(Scitala)/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IB_1_Scitala_
+{
+    public class ScytaleCipher
+    {
+        static public string Encrypt(string text, int d)
+        {
+            var k = text.Length % d;
+            if (k > 0)
+            {
+                //дополняем строку пробелами
+                text += new string(' ', d - k);
+            }
+
+            var column = text.Length / d;
+            var result = "";
+
+            for (int i = 0; i < column; i++)
+            {
+                for (int j = 0; j < d; j++)
+                {
+                    result += text[i + column * j].ToString();
+                }
+            }
+
+            return result;
+        }
+
+        static public string Decrypt(string text, int d)
+        {
+            var column = text.Length / d;
+            var symbols = new char[text.Length];
+            int index = 0;
+            for (int i = 0; i < column; i++)
+            {
+                for (int j = 0; j < d; j++)
+                {
+                    symbols[i + column * j] = text[index];
+                    index++;
+                }
+            }
+
+            return string.Join("", symbols);
+        }
+
+        static public string Breaking(string text)
+        {
+            int d = 1;
+            bool stop = false;
+            string result = string.Empty;
+            while (!stop)
+            {
+                result = Decrypt(text, d);
+                Console.WriteLine("Диаметр " + d + "; \nРасшифрованный текст:\n" + result);
+                Console.WriteLine("Расшифровка правильная? (true - да, false - нет)");
+                stop = Convert.ToBoolean(Console.ReadLine());
+                d++;
+            }
+            return result;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var scytale = new ScytaleCipher();
+            //Console.Write("Введите текст сообщения: ");
+            //var message = Console.ReadLine();
+            //Console.Write("Введите диаметр цилиндра: ");
+            //var diameter = Convert.ToInt32(Console.ReadLine());
+            //var encText = scytale.Encrypt(message, diameter);
+            //Console.WriteLine("Зашифрованный текст: {0}", encText);
+            //Console.WriteLine("Расшифрованный текст: {0}", scytale.Decrypt(encText, diameter));
+            //Console.ReadLine();
+
+            while (true)
+            {
+                Console.WriteLine("\n----------------\n1 - Шифровка\n2 - Расшифровка\n3 - Взлом без ключа");
+                int key = int.Parse(Console.ReadLine());
+                switch(key)
+                {
+                    case 1:
+                        Console.Write("Введите сообщение: ");
+                        string message1 = Console.ReadLine();
+                        Console.Write("Введите диаметр циллиндра: ");
+                        int diameter1 = int.Parse(Console.ReadLine());
+                        string encText = ScytaleCipher.Encrypt(message1, diameter1);
+                        Console.WriteLine("Зашифрованный текст: \n|" + encText + "|");
+                        Console.WriteLine("Расшифровать данный текст? (true - да, false - нет)");
+                        bool go = Convert.ToBoolean(Console.ReadLine());
+                        if (go)
+                            Console.WriteLine("Расшифрованный текст: \n" + ScytaleCipher.Decrypt(encText, diameter1));
+                        break;
+
+                    case 2:
+                        Console.Write("Введите зашифрованный текст: ");
+                        string message2 = Console.ReadLine();
+                        Console.Write("Введите диметр циллиндра: ");
+                        int diameter2 = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Расшифрованный текс: \n" + ScytaleCipher.Decrypt(message2, diameter2));
+                        break;
+
+                    case 3:
+                        Console.Write("Введите зашифрованный текст (обратите внимание на пробелы): ");
+                        string message3 = Console.ReadLine();
+                        ScytaleCipher.Breaking(message3);
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ InfBez_2(Vigenère)/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfBez_2_Vigenère_
+{
+    class Program
+    {
+        static char[] characters = new char[] { 'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И',
+                                                'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С',
+                                                'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ь', 'Ы', 'Ъ',
+                                                'Э', 'Ю', 'Я', ' ', '1', '2', '3', '4', '5', '6', '7',
+                                                '8', '9', '0', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+                                                'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 
+                                                'V', 'W', 'X', 'Y', 'Z', ',', '.', '?', '-', '!', ':', ';', '(', ')'};
+
+        static int N = characters.Length;
+
+        static private string Decode(string input, string keyword)
+        {
+            input = input.ToUpper();
+            keyword = keyword.ToUpper();
+
+            string result = "";
+
+            int keyword_index = 0;
+
+            foreach (char symbol in input)
+            {
+                int p = (Array.IndexOf(characters, symbol) + N -
+                    Array.IndexOf(characters, keyword[keyword_index])) % N;
+
+                result += characters[p];
+
+                keyword_index++;
+
+                if ((keyword_index + 1) == keyword.Length)
+                    keyword_index = 0;
+            }
+
+            return result;
+        }
+
+        static public string Encode(string input, string keyword)
+        {
+            input = input.ToUpper();
+            keyword = keyword.ToUpper();
+
+            string result = "";
+
+            int keyword_index = 0;
+
+            foreach (char symbol in input)
+            {
+                int c = (Array.IndexOf(characters, symbol) +
+                    Array.IndexOf(characters, keyword[keyword_index])) % N;
+
+                result += characters[c];
+
+                keyword_index++;
+
+                if ((keyword_index + 1) == keyword.Length)
+                    keyword_index = 0;
+            }
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("1 - Зашифровать сообщение\n2 - Расшифровать сообщение");
+                string num = Console.ReadLine();
+                switch (num)
+                {
+                    case "1":
+                        Console.Write("Введите ключ:  ");
+                        string key1 = Console.ReadLine();
+                        Console.Write("Введите сообщение:  ");
+                        string message = Console.ReadLine();
+                        string encryptMessage1 = Encode(message, key1);
+                        Console.WriteLine($"Зашифрованное сообщение: \n|{encryptMessage1}|");
+                        Console.Write("Расшифровать данное сообщение?(да/нет):  ");
+                        string go = Console.ReadLine();
+                        if (go.ToUpper() == "ДА")
+                            Console.WriteLine($"Расшифрованное сообщение: \n{Decode(encryptMessage1, key1)}");
+                        break;
+
+                    case "2":
+                        Console.Write("Введите ключ:  ");
+                        string key2 = Console.ReadLine();
+                        Console.Write("Введите зашифрованное сообщение:  ");
+                        string encryptMessage2 = Console.ReadLine();
+                        Console.WriteLine($"Расшифрованное сообщение: \n{Decode(encryptMessage2, key2)}");
+                        break;
+
+                    default:
+                        break;
+                }
+                Console.Write("\nНажмите любую клавишу...");
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
